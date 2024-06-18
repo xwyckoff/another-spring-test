2ebb429294881603e76c577702c89fcfbc6d57a4
Xander <xander.wyckoff@gmail.com>
2024-06-18 15:58:28 -0400

    created author controller

---
+++ src/main/java/com/example/demo/controllers/AuthorController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.example.demo.domain.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}

5fe3a8e7f433786d30376f239b39565f5ac4c0d2
Xander <xander.wyckoff@gmail.com>
2024-06-17 20:35:07 -0400

    created basic POJOs

---
+++ src/main/java/com/example/demo/domain/Author.java
@@ -0,0 +1,45 @@
+package com.example.demo.domain;
+
+import java.util.Set;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+    
+    public Author() {
+        
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    
+}

c0bed0a68db18c327c8faa52ea1aa4cc73be20dc
Xander <xander.wyckoff@gmail.com>
2024-06-18 16:18:07 -0400

    added template to display authors

--- src/main/java/com/example/demo/controllers/AuthorController.java
+++ src/main/java/com/example/demo/controllers/AuthorController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.domain.repositories.AuthorRepository;
 
@@ -13,6 +14,7 @@ public class AuthorController {
         this.authorRepository = authorRepository;
     }
 
+    @GetMapping("/authors")
     public String getAuthors(Model model) {
         model.addAttribute("authors", authorRepository.findAll());
         return "authors";
